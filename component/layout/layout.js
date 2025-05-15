c34f99edf376f8b9964e9f5bd38174c4b5dae013
Jatinrao78 <your-email@example.com>
2025-05-15 12:37:35 +0530

    Your update message

--- component/layout/layout.js
+++ component/layout/layout.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Nav from './navbar/navbar.js'
+import Sidebar from '../components/sidebar/Sidebar.js';
 import Styles from './layout.module.css'
 
 const Layout = ({children, formName}) => {
   return (
     <div className={Styles.layoutContainer}>
-        <header className={Styles.navbar}>
-            <Nav formName={formName}/>
-        </header>
-        <main className={Styles.main}>{children}</main>
-        <footer className={Styles.footer}>
-
-        </footer>
+        <Sidebar />
+        <div className={Styles.mainContent}>
+          <header className={Styles.navbar}>
+              <Nav formName={formName}/>
+          </header>
+          <main className={Styles.main}>{children}</main>
+          <footer className={Styles.footer}>
+          </footer>
+        </div>
     </div>    
   )
 }
